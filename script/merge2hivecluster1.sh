40119f163b96dbc6ba41fd25d6da5b3a92a5e5fd
sevenseablue <sevenseablue@gmail.com>
2019-10-28 18:10:17 +0800

    update package name

--- script/merge2hivecluster1.sh
+++ script/merge2hivecluster1.sh
@@ -1,4 +1,7 @@
-export SOURCE_NAME=exchange_db
-export DEST_NAME=online_cluster1
+#!/usr/bin/env bash
 
-/home/hadoop/java-current/jre/bin/java -cp "./hive-tools-current.jar" com.netease.hivetools.apps.MetaDataMerge --s=$SOURCE_NAME --d=$DEST_NAME
+echo "fk"
+export SOURCE_NAME=test2
+export DEST_NAME=test1
+
+java -cp "./hive-tools-current.jar" com.netease.hivetools.apps.MetaDataMerge --s=$SOURCE_NAME --d=$DEST_NAME
